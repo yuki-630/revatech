7a5fad384d7bf9d457d9be1ef9b0618062f1cecf
EC2 Default User <ec2-user@ip-172-31-15-10.ap-northeast-1.compute.internal>
2024-07-11 00:11:18 +0900

    8-3 ブログ投稿詳細の表示

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,6 @@ use App\Http\Controllers\PostController;
 |
 */
 Route::get('/', [PostController::class, 'index']);
+
+Route::get('/posts/{post}', [PostController::class ,'show']);
+// '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメゾットを実行する
