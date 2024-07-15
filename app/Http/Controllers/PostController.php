4b8f178bc5974dec3d48db7df6c9dbfcaf0b36f6
EC2 Default User <ec2-user@ip-172-31-15-10.ap-northeast-1.compute.internal>
2024-07-15 15:13:53 +0900

    8-4　ブログ投稿の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
+
 
 class PostController extends Controller
 {
@@ -22,6 +23,18 @@ class PostController extends Controller
     {
         return view('posts.show')->with(['post' => $post]);
       //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+    }
+    
+    public function create()
+    {
+        return view('posts.create');
+    }
+    
+    public function store(PostRequest $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
     }
 }
 ?>
